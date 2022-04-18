81c958d57075a243951067d3e200f2d9af2393dc
wormyn <87888767+wormyn@users.noreply.github.com>
2022-04-18 12:22:17 -0700

    accounts models added

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,40 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
   // DO YOUR MAGIC
+  //SELECT * FROM accounts;
+  return db('accounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  // SELECT * FROM accounts WHERE id = ?;
+  return db('accounts')
+    .where('id', id)
+    .first();
 }
 
-const create = account => {
+const create = async account => {
   // DO YOUR MAGIC
+  // INSERT INTO accounts (name, budget) VALUES(? ,?);
+  let [id] = await db('accounts')
+    .insert(account);
+  return getById(id);
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
+  // UPDATE accounts SET name = ?, budget = ? WHERE id = ?;
+  await db('accounts').where('id', id).update(account);
+  return getById(id);
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  // DELETE FROM accounts WHERE id = ?;
+  let result = await getById(id);
+  await db('accounts').where('id', id).del();
+  return result;
 }
 
 module.exports = {
